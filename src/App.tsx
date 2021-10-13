2b6bc2c2b28c15b48d7d6ec88563586fc6cfa522
Juliana Baez <juliana.baez@outlook.com>
2021-10-13 20:25:12 -0300

    start Login page -task 1 and 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import './App.css';
 import 'bootswatch/dist/quartz/bootstrap.min.css'
+import {Login, Dashboard} from './pages'
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login/>  
+      <Dashboard/>  
     </div>
   );
 }
