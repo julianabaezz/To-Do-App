33450801bf701c541fc0809e9fe70cca3aaac305
Juliana Baez <juliana.baez@outlook.com>
2021-10-14 19:00:33 -0300

    import favicon/ create layout folder

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import { FormEvent } from "react"
 
 const Login = () =>{
     
-    const handleSubmit =(e: FormEvent) =>{
+    const handleSubmit =(e:FormEvent) =>{
         e.preventDefault();
         console.log('Entré')
 
@@ -10,17 +10,17 @@ const Login = () =>{
 
 
     return(
-    <form action="" onSubmit={handleSubmit}>
-        <div className="form-group">
+    <form action="" onSubmit={handleSubmit} className= "container">
+        <legend>Login</legend>
+        <div className="form-group row">
             <label htmlFor="exampleInputEmail1" className="form-label mt-4">Email address</label>
             <input required type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-        <div className="form-group">
+        <div className="form-group row">
             <label htmlFor="exampleInputPassword1" className="form-label mt-4">Password</label>
             <input required type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+        <button type="submit" className="btn btn-primary mt-4">Submit</button>
     </form>
     )
 }
